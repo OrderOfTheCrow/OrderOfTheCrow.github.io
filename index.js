375b891b3d15e4d4caa9a108e15bcbd49c3318ad
Luca <lucagough7@gmail.com>
2025-03-27 19:02:25 +0000

    testing pdfs

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-ARTICLE_DIR = "articles"
+const ARTICLE_DIR = "articles"
+const OWNER = "LucaG-17"
+const REPO = "SETI_STA"
 
-
-window.onload = function populateArticles() {
-    
+window.onload = async function populateArticles() {
+    const articleUrl =  `https://api.github.com/repos/${OWNER}/${REPO}/contents/${ARTICLE_DIR}`;
+    try {
+        const response = await fetch(articleUrl)
+        const files = await response.json()
+        console.log(files)
+    } catch (error) {
+        console.error("Error fetching article list:", error);
+    }
 }

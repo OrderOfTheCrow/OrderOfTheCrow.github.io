b3428f0934f82e5a4abb1f2a23de93e4e80fdb3a
Luca Gough <lucagough7@gmail.com>
2025-03-29 14:37:03 +0000

    added fancy scrolling

--- script.js
+++ script.js
@@ -35,3 +35,12 @@ window.onload = async function populateArticles() {
         console.error("Error fetching article list:", error);
     }
 }
+
+function onClickInternalLink(id) {
+    var element = document.getElementById(id);
+    element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest"
+    });
+}
